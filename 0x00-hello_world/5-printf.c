869c65db8a510f17ec97529b9c50e21e5d0f4cf8
eseee0 <ada.oyugbo@gmail.com>
2023-02-16 14:20:06 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line usinf printf function
+ * main - A program that prints a line with printf function
+ * Description: 'the program prints a line with printf'
  * Return : 0 (always)
  */
 int main(void)
